cdcf89b46093ea01421096d77c29fcf2f35878f5
xushuangshuang <xushuangshuang1@gmail.com>
2014-02-21 12:08:37 +0800

    char function

--- InternTest.java
+++ InternTest.java
@@ -9,5 +9,11 @@ public class InternTest
 		Integer a = new Integer(b);
 		int c= a.intValue();
 		System.out.println(c);
+		
+
+		char ch = 'a';
+		Character dd = new Character(ch);
+		char ch1 =dd.charValue();
+		System.out.println(ch1);
 	}
 }
