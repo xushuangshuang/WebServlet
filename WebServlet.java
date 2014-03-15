8fee5b015ecc3b0bf227fb14d81c161cc8f4cbfb
xushuangshuang <xushuangshuang1@gmail.com>
2014-03-15 16:29:59 +0800

    尝试连接数据库

--- WebServlet.java
+++ WebServlet.java
@@ -1,19 +1,56 @@
 package com.bodejidi;
-
-import java.servlet.http.HttpServlet;
-import java.servlet.http.HttpServletRequest;
-import java.servlet.http.HttpServlerResponce;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
 
 public class WebServlet extends HttpServlet
 {
-	public void doget(HttpServletRequst req, HttpServletResponce resp)
+	public void doget(HttpServletRequest req, HttpServletResponse resp)
 	{
 	
 	}
 
-	public void dopust(HttpServletRequest req, HttpServletResponce resp)
+	public void dopust(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
 	{
+		resp.setContentType("text/html; charset=UTF-8");
 		
+	
+	/*
+	 * int age = 0;
+		String name = req.getParameter("name");
+		String age = req.getParameter(age);
+		*/
+		
+       		try
+       		{
+             		  Class.forName("com.mysql.jdbc.Driver").newInstance();
+       		 } 
+		catch (Exception ex)
+       		{
+            
+       		 }
+				
+		Connection conn = null;
+				
+		try
+		{
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/test?"
+							+ "username = root" + "password =");
+
+			resp.getWriter().println("success");	
+		}
+		catch(SQLException ex)
+		{
+			System.out.println("SQLException: " + ex.getMessage());
+           	 	System.out.println("SQLState: " + ex.getSQLState());
+            		System.out.println("VendorError: " + ex.getErrorCode());
+           		resp.getWriter().println("Error!");
+		}	
 	}
 
 }
