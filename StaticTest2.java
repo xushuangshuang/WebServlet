583c24effa26ae4ba82ae1d883ace6b55c66492f
xushuangshuang <xushuangshuang1@gmail.com>
2014-03-15 15:31:09 +0800

    建立了两个方法，准备链接数据库，发现没有网页

---
+++ StaticTest2.java
@@ -0,0 +1,16 @@
+public class StaticTest2
+{
+	public static void main(String[] args)
+	{
+		//MyStatic2 mystatic2 = new Mystatic2();
+		MyStatic2.output();
+	}
+}
+
+class MyStatic2
+{
+	public static void output()
+	{
+		System.out.println("outpuut");
+	}
+}
---
+++ WebServlet.java
@@ -0,0 +1,19 @@
+package com.bodejidi;
+
+import java.servlet.http.HttpServlet;
+import java.servlet.http.HttpServletRequest;
+import java.servlet.http.HttpServlerResponce;
+
+public class WebServlet extends HttpServlet
+{
+	public void doget(HttpServletRequst req, HttpServletResponce resp)
+	{
+	
+	}
+
+	public void dopust(HttpServletRequest req, HttpServletResponce resp)
+	{
+		
+	}
+
+}
